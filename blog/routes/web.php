e5cb28e6d56cbd3218f2fe7be66e87c285a61a8f
evelingcastillon <evelingcastillon0304@gmail.com>
2021-07-16 22:11:30 +0200

    riprova

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -14,4 +14,4 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', 'PageController@index')->name('home');
-Route::get('/travels', 'TravelController@index')->name('travels');
+Route::get('travels', 'TravelController@index')->name('travels');
